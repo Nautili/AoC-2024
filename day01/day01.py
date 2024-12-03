be0a0b2f921a861ea6dccc6519bd66aa3258df86
nautili <lear.vg@gmail.com>
2024-12-02 22:42:57 -0800

    Format earlier days
    Add formatting since we're using an automatic linter now.

--- day01/day01.py
+++ day01/day01.py
@@ -1,14 +1,16 @@
 import sys
 from collections import Counter
 
+
 def main():
     with open(sys.argv[1]) as f:
         lines = [[int(val) for val in line.split()] for line in f.readlines()]
         # part 1
         first_line, second_line = zip(*lines)
-        print(sum(abs(a - b) for a, b in zip(sorted(first_line), sorted(second_line))))
+        print(sum(abs(a - b)
+              for a, b in zip(sorted(first_line), sorted(second_line))))
 
-        #part 2
+        # part 2
         counts = Counter(second_line)
         print(sum(a * counts[a] for a in first_line))
 
--- day02/day02.py
+++ day02/day02.py
@@ -1,8 +1,10 @@
 import sys
 
+
 def is_good_report(line):
     diffs = [a - b for a, b in zip(line[:-1], line[1:])]
     return all(0 < diff < 4 for diff in diffs) or all(-4 < diff < 0 for diff in diffs)
+
 
 def is_good_report_except_one(line):
     for i in range(len(line)):
@@ -11,6 +13,7 @@ def is_good_report_except_one(line):
         if all(0 < diff < 4 for diff in diffs) or all(-4 < diff < 0 for diff in diffs):
             return True
     return False
+
 
 def main():
     with open(sys.argv[1]) as f:
