216d284e0a4d57d8071d30fea10505c2009c6cd2
nautili <lear.vg@gmail.com>
2024-12-04 07:39:05 -0800

    More cleanup
    Remove excess padding since that should be short circuited in each
    check. Format some of the array accesses for readability.

--- day04/day04.py
+++ day04/day04.py
@@ -6,15 +6,15 @@ def check_location(grid, row, col):
     hits = 0
     for row_diff in range(-1, 2):
         for col_diff in range(-1, 2):
-            hits += all(grid[row + i * row_diff][col + i *
-                        col_diff] == c for i, c in enumerate('XMAS'))
+            hits += all(grid[row + i * row_diff]
+                            [col + i * col_diff] == c for i, c in enumerate('XMAS'))
     return hits
 
 
 def check_xmas(grid):
     total = 0
-    for row in range(3, len(grid) - 3):
-        for col in range(3, len(grid[row]) - 3):
+    for row in range(1, len(grid) - 1):
+        for col in range(1, len(grid[row]) - 1):
             total += check_location(grid, row, col)
     return total
 
@@ -22,15 +22,15 @@ def check_xmas(grid):
 def get_middles(grid, row, col, middles):
     for row_diff in [-1, 1]:
         for col_diff in [-1, 1]:
-            if all(grid[row + i * row_diff][col + i *
-                                            col_diff] == c for i, c in enumerate('MAS')):
+            if all(grid[row + i * row_diff]
+                       [col + i * col_diff] == c for i, c in enumerate('MAS')):
                 middles[(row + row_diff, col + col_diff)] += 1
 
 
 def check_mas(grid):
     middles = defaultdict(int)
-    for row in range(3, len(grid) - 3):
-        for col in range(3, len(grid[row]) - 3):
+    for row in range(1, len(grid) - 1):
+        for col in range(1, len(grid[row]) - 1):
             get_middles(grid, row, col, middles)
     return sum(v > 1 for v in middles.values())
 
@@ -38,8 +38,8 @@ def check_mas(grid):
 def main():
     with open(sys.argv[1]) as f:
         lines = [line.strip() for line in f.readlines()]
-        lines = ["###" + line + "###" for line in lines]
-        lines = ["#" * len(lines[0])] * 3 + lines + ["#" * len(lines[0])] * 3
+        lines = ["#" + line + "#" for line in lines]
+        lines = ["#" * len(lines[0])] + lines + ["#" * len(lines[0])]
         print(check_xmas(lines))
         print(check_mas(lines))
 
