47059e37a698e0458d8af4df039d3e9f97b0fe44
nautili <lear.vg@gmail.com>
2024-12-05 21:52:13 -0800

    Refactor shared code and minor optimization
    Another day of combining differences with multiple return values. I
    still don't really like this pattern, but it is fine for now.
    
    Switch to only checking values in the initial path. This speeds up the
    program significantly.

--- day06/day06.py
+++ day06/day06.py
@@ -15,48 +15,32 @@ def get_traveled_path(grid):
     seen = set()
     while row + row_d >= 0 and row + row_d < len(grid) and \
             col + col_d >= 0 and col + col_d < len(grid[0]):
-        seen.add((row, col))
-        while grid[row + row_d][col + col_d] == '#':
-            row_d, col_d = col_d, -row_d  # rotate
-        row += row_d
-        col += col_d
-    # +1 here to count the last edge. There only way to reach the edge is when
-    # leaving, so this should be safe
-    return len(seen) + 1
-
-
-def path_has_loop(grid):
-    row, col = find_start(grid)
-    row_d = -1
-    col_d = 0
-    seen = set()
-    while row + row_d >= 0 and row + row_d < len(grid) and \
-            col + col_d >= 0 and col + col_d < len(grid[0]):
         if (row, col, row_d, col_d) in seen:
-            return True
+            return set((row, col) for row, col, _, _ in seen), True
         seen.add((row, col, row_d, col_d))
         while grid[row + row_d][col + col_d] == '#':
             row_d, col_d = col_d, -row_d  # rotate
         row += row_d
         col += col_d
-    return False
+    seen.add((row, col, row_d, col_d))
+    return set((row, col) for row, col, _, _ in seen), False
 
 
 def count_possible_loops(grid):
     total = 0
-    for row, grid_row in enumerate(grid):
-        for col, val in enumerate(grid_row):
-            if val == '.':
-                grid[row][col] = '#'
-                total += path_has_loop(grid)
-                grid[row][col] = '.'
+    traveled = get_traveled_path(grid)[0]
+    for row, col in traveled:
+        if grid[row][col] == '.':  # avoid the start
+            grid[row][col] = '#'
+            total += get_traveled_path(grid)[1]
+            grid[row][col] = '.'
     return total
 
 
 def main():
     with open(sys.argv[1]) as f:
         lines = [list(line.strip()) for line in f.readlines()]
-        print(get_traveled_path(lines))
+        print(len(get_traveled_path(lines)[0]))
         print(count_possible_loops(lines))
 
 
