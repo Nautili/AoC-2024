545983e24e6be04645553a614bf1983bafbd62fc
nautili <lear.vg@gmail.com>
2024-12-06 21:31:33 -0800

    Refactor day 7
    Use a flag to combine the functions. Add an early exit (although it
    doesn't seem to affect the time significantly).

--- day07/day07.py
+++ day07/day07.py
@@ -1,42 +1,33 @@
 import sys
 
 
-def is_reachable(target, curVal, values):
+def is_reachable(target, curVal, values, check_concat=False):
+    if (curVal > target):
+        return False
+
     if len(values) == 0:
         return curVal == target
 
-    return is_reachable(target, curVal + values[0], values[1:]) or \
-        is_reachable(target, curVal * values[0], values[1:])
+    return is_reachable(target, curVal + values[0], values[1:], check_concat) or \
+        is_reachable(target, curVal * values[0], values[1:], check_concat) or \
+        (check_concat and is_reachable(target,
+                                       int(str(curVal) + str(values[0])),
+                                       values[1:],
+                                       check_concat))
 
 
-def count_reachable(equations):
+def count_reachable(equations, check_concat=False):
     return sum(equation[0] for equation in equations
-               if is_reachable(equation[0], equation[1], equation[2:]))
-
-
-def is_concat_reachable(target, curVal, values):
-    if len(values) == 0:
-        return curVal == target
-
-    return is_concat_reachable(target, curVal + values[0], values[1:]) or \
-        is_concat_reachable(target, curVal * values[0], values[1:]) or \
-        is_concat_reachable(target, int(
-            str(curVal) + str(values[0])), values[1:])
-
-
-def count_concat_reachable(equations):
-    return sum(equation[0] for equation in equations
-               if is_concat_reachable(equation[0], equation[1], equation[2:]))
+               if is_reachable(equation[0], equation[1], equation[2:], check_concat))
 
 
 def main():
     with open(sys.argv[1]) as f:
         lines = [line.strip() for line in f.readlines()]
-    equations = []
-    for line in lines:
-        equations += [[int(num.strip(':')) for num in line.split()]]
+    equations = [[int(num.strip(':')) for num in line.split()]
+                 for line in lines]
     print(count_reachable(equations))
-    print(count_concat_reachable(equations))
+    print(count_reachable(equations, check_concat=True))
 
 
 if __name__ == '__main__':
