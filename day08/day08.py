5d791a1e74b2d74b85e51e3bed70b55a0820e229
nautili <lear.vg@gmail.com>
2024-12-07 21:51:59 -0800

    Add day 8
    This is a little cleaned up. There is probably a clever way to combine
    the two main functions, but most of the ideas I tried were at least a
    little uglier than this.

---
+++ day08/day08.py
@@ -0,0 +1,65 @@
+import sys
+from collections import defaultdict
+
+
+def in_bounds(grid, loc):
+    row, col = loc
+    return row >= 0 and col >= 0 and row < len(grid) and col < len(grid[0])
+
+
+def add(a, b):
+    return tuple(a_val + b_val for a_val, b_val in zip(a, b))
+
+
+def sub(a, b):
+    return tuple(a_val - b_val for a_val, b_val in zip(a, b))
+
+
+def mul(m, a):
+    return tuple(m * a_val for a_val in a)
+
+
+def add_single_antinode(left, right, grid, antinodes):
+    delta = sub(left, right)
+    if in_bounds(grid, add(left, delta)):
+        antinodes.add(add(left, delta))
+    if in_bounds(grid, sub(right, delta)):
+        antinodes.add(sub(right, delta))
+
+
+def add_all_antinodes(left, right, grid, antinodes):
+    delta = sub(left, right)
+    k = 0
+    while in_bounds(grid, add(left, mul(k, delta))):
+        antinodes.add(add(left, mul(k, delta)))
+        k += 1
+    k = 0
+    while in_bounds(grid, sub(right, mul(k, delta))):
+        antinodes.add(sub(right, mul(k, delta)))
+        k += 1
+
+
+def get_antinodes(grid, update_antinodes):
+    antennae = defaultdict(list)
+    for i, row in enumerate(grid):
+        for j, val in enumerate(row):
+            if val != '.':
+                antennae[val] += [(i, j)]
+
+    antinodes = set()
+    for _, locs in antennae.items():
+        for l in range(len(locs) - 1):
+            for r in range(l + 1, len(locs)):
+                update_antinodes(locs[l], locs[r], grid, antinodes)
+    return antinodes
+
+
+def main():
+    with open(sys.argv[1]) as f:
+        lines = [line.strip() for line in f.readlines()]
+    print(len(get_antinodes(lines, add_single_antinode)))
+    print(len(get_antinodes(lines, add_all_antinodes)))
+
+
+if __name__ == '__main__':
+    main()
