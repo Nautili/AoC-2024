97b7ee99c5618805f47d190e57e5956f8d43df03
nautili <lear.vg@gmail.com>
2024-12-21 23:34:29 -0800

    Add day 22
    Surprisingly straightforward. The only clever parts are using bitwise
    operations everywhere and finding a compact way to encode a sequence as
    a single 20 bit value. I'm not sure this actually has a significant
    effect since storing 4 integer values (or even 4 chars in a different
    language) isn't significantly more. There are also only 19^4 sequences,
    which is small, so this is a micro-optimization at best.

---
+++ day22/day22.py
@@ -0,0 +1,52 @@
+import sys
+from collections import defaultdict
+
+
+def get_next_secret_number(s):
+    mask = (1 << 24) - 1
+    s = ((s << 6) ^ s) & mask
+    s = ((s >> 5) ^ s)
+    s = ((s << 11) ^ s) & mask
+    return s
+
+
+def get_secret_num_sum(vals, n):
+    total = 0
+    for val in vals:
+        for _ in range(n):
+            val = get_next_secret_number(val)
+        total += val
+    return total
+
+
+def get_best_sequence(s, n, counts):
+    seen_sequences = set()
+    cur_key = 0
+    cur_val = s % 10
+    for i in range(n):
+        s = get_next_secret_number(s)
+        cur_diff = (s % 10) - cur_val
+        cur_val = s % 10
+        cur_key = ((cur_key << 5) | (cur_diff & 0b11111)) & ((1 << 20) - 1)
+
+        if i >= 3 and cur_key not in seen_sequences:
+            seen_sequences.add(cur_key)
+            counts[cur_key] += cur_val
+
+
+def get_best_sequences(vals, n):
+    counts = defaultdict(int)
+    for val in vals:
+        get_best_sequence(val, n, counts)
+    return max(counts.values())
+
+
+def main():
+    with open(sys.argv[1]) as f:
+        vals = [int(val.strip()) for val in f.readlines()]
+    print(get_secret_num_sum(vals, 2000))
+    print(get_best_sequences(vals, 2000))
+
+
+if __name__ == '__main__':
+    main()
